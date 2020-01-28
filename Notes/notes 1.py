f70be95b435c5d3a3b1da1b47f53ca5b298f2dbc
mgardner2022 <mgardner@fwparker.org>
2020-01-28 13:25:11 -0600

    Added import notes

---
+++ Notes/notes 1.py
@@ -0,0 +1,14 @@
+import math
+import random as r
+from math import *
+from Notes import settings
+from Notes.settings import *
+
+if __name__ == "__main__":
+    print(math)
+    print(r.randrange(1, 7))
+    print(pi)
+    print(settings.a)
+    print(a)
+    print(settings. double(a))
+
